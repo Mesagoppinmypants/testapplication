6b0b93d5dac7891cb675a11e9d7f230e048ba811
Iosnowore <pressinger.gene66@gmail.com>
2015-07-10 00:38:33 -0400

    Button correction revamp

--- TestApplication/TestApplication/Form1.Designer.cs
+++ TestApplication/TestApplication/Form1.Designer.cs
@@ -28,43 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkbox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(139, 261);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "YouTube";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(263, 261);
+            this.button2.Location = new System.Drawing.Point(139, 261);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Facebook";
+            this.button2.TabIndex = 0;
+            this.button2.Text = "YouTube";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 261);
+            this.button3.Location = new System.Drawing.Point(263, 261);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Twitter";
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Facebook";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Twitter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button3_Click);
             // 
             // checkBox1
             // 
@@ -77,16 +77,16 @@
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // checkbox2
+            // checkBox2
             // 
-            this.checkbox2.AutoSize = true;
-            this.checkbox2.Location = new System.Drawing.Point(105, 90);
-            this.checkbox2.Name = "checkbox2";
-            this.checkbox2.Size = new System.Drawing.Size(106, 17);
-            this.checkbox2.TabIndex = 4;
-            this.checkbox2.Text = "Enable YouTube";
-            this.checkbox2.UseVisualStyleBackColor = true;
-            this.checkbox2.CheckedChanged += new System.EventHandler(this.checkbox2_CheckedChanged);
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(105, 90);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(106, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Enable YouTube";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkbox2_CheckedChanged);
             // 
             // checkBox3
             // 
@@ -105,11 +105,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(350, 318);
             this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkbox2);
+            this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "<DJO>";
             this.ResumeLayout(false);
@@ -119,11 +119,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkbox2;
+        private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
     }
 }
--- TestApplication/TestApplication/Form1.cs
+++ TestApplication/TestApplication/Form1.cs
@@ -32,11 +32,6 @@ namespace TestApplication
             MessageBox.Show("This function is coming soon.", "Twitter Button");
         }
 
-        private void checkBox3_CheckedChanged(object sender, EventArgs e)
-        {
-            Box3_Checked();
-        }
-
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             Box1_Checked();
@@ -47,19 +42,24 @@ namespace TestApplication
             Box2_Checked();
         }
 
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            Box3_Checked();
+        }
+
         private void Box1_Checked()
         {
-            button1.Enabled = checkBox1.Checked;
+            button2.Enabled = checkBox1.Checked;
         }
 
         private void Box2_Checked()
         {
-            button2.Enabled = checkbox2.Checked;
+            button3.Enabled = checkBox2.Checked;
         }
 
         private void Box3_Checked()
         {
-            button3.Enabled = checkBox3.Checked;
+            button1.Enabled = checkBox3.Checked;
         }
     }
 }
