39e3ebdcf0c5c8db3cf474e619a28c4a3173f541
Iosnowore <pressinger.gene66@gmail.com>
2015-07-10 14:31:01 -0400

    Disabled background until it's fixed

--- TestApplication/TestApplication/Form1.Designer.cs
+++ TestApplication/TestApplication/Form1.Designer.cs
@@ -110,7 +110,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::TestApplication.Properties.Resources.Background;
+            //this.BackgroundImage = global::TestApplication.Properties.Resources.Background;
             this.ClientSize = new System.Drawing.Size(350, 318);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
