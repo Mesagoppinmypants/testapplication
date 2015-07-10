d9029720ab2d961e6f9fa27280912dca60052825
Iosnowore <pressinger.gene66@gmail.com>
2015-07-10 18:01:10 -0400

    added notes

--- TestApplication/TestApplication/Form6.cs
+++ TestApplication/TestApplication/Form6.cs
@@ -17,47 +17,56 @@ namespace TestApplication
             InitializeComponent();
         }
 
+        // Disable sound effects
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             Do_Checked_checkBox1();
         }
 
+        // Disables music
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
             Do_Checked_checkBox2();
         }
 
+        // Enables nightmode
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
             Do_Checked_checkBox3();
         }
 
+        
         private void Do_Checked_checkBox1()
         {
             MessageBox.Show("This function is coming soon.", "Music Enable/Disable");
         }
 
+        
         private void Do_Checked_checkBox2()
         {
             MessageBox.Show("This function is coming soon.", "Night Mode");
         }
 
+        
         private void Do_Checked_checkBox3()
         {
             MessageBox.Show("This function is coming soon.", "All Sound Enable/Disable");
         }
 
+        // When the user clicks the "apply" button
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
             MessageBox.Show("The save settings function has not been implemented yet.", "Saving Settings");
         }
 
+        // This is when the user changes the language option.
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             
         }
 
+        // This is when the page closes
         private void Form6_FormClosed(object sender, FormClosedEventArgs e)
         {
            // Properties.Settings.Default.textbox = checkBox1.Checked;
